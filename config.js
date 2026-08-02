fb4e7be23f2538aa5db38c7f5a076358f21c6a53
WhoSia <71053804+WhoSia@users.noreply.github.com>
2026-08-02 09:44:34 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,7 @@ window.KSGT_CONFIG = Object.freeze({
   clientVersion: "0.3.23-human-pilot-pages-rc3",
   consentVersion: "H0-ko-v3",
 
-  // RC3 동의문과 Collector를 다시 검사할 때까지 안전 잠금 상태입니다.
-  launchMode: "prelaunch", // prelaunch | pilot
+  launchMode: "pilot", // prelaunch | pilot
 
   collectorUrl: "https://script.google.com/macros/s/AKfycbzMG73a__B3vinn8bPT3-eyroPuMJfTKmHpkMvjKZNSqGmimmynmnZq7HFTWj3_VWMj/exec",
   contact: "설문 링크를 전달한 연구자에게 문의해 주세요.",
